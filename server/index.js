e54bd50b1b16d0ca155f0e24f7ef7cc9bc2591a3
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-08-20 20:17:30 +0530

    HM | adding nessasary files to server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 
-const dbConfig = require("./db");
+const dbConfig = require("./config/db");
 
 app.use(express.json());
 
