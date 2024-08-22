913c99179501718edc358566adaa4258b230d165
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-08-22 09:20:54 +0530

    HM | Profile update

---
+++ client/src/components/User/RoomBookings.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function RoomBookings() {
+    return <div>
+        <h1>Room Bookings</h1>
+        <p>Room bookings will be displayed here.</p>
+    </div>;
+}
+
+export default RoomBookings;
--- client/src/pages/UserProfilePage.jsx
+++ client/src/pages/UserProfilePage.jsx
@@ -1,27 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { message } from "antd"; // Import Antd message component
+import { message, Tabs, ConfigProvider } from "antd"; // Import Antd message component
+import RoomBookings from "../components/User/RoomBookings";
+import EventBookings from "../components/User/EventBookings";
+import FoodOrders from "../components/User/FoodOrders";
+import ParkingBookings from "../components/User/ParkingBookings";
+import LeaveDetails from "../components/User/LeaveDetails";
 
 function UserProfilePage() {
-
     const [user, setUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
+    const [activeTabKey, setActiveTabKey] = useState("1");
     const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        username: ''
+        firstName: "",
+        lastName: "",
+        email: "",
+        username: "",
     });
 
+    // Handle tab change
+    const onTabChange = (key) => {
+        setActiveTabKey(key);
+    };
+
     useEffect(() => {
-        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        const currentUser = JSON.parse(localStorage.getItem("currentUser"));
         if (currentUser) {
             setUser(currentUser);
             setFormData({
                 firstName: currentUser.firstName,
                 lastName: currentUser.lastName,
                 email: currentUser.email,
-                username: currentUser.username
+                username: currentUser.username,
             });
         }
     }, []);
@@ -30,7 +40,7 @@ function UserProfilePage() {
         const { name, value } = e.target;
         setFormData({
             ...formData,
-            [name]: value
+            [name]: value,
         });
     };
 
@@ -40,23 +50,26 @@ function UserProfilePage() {
 
     const handleSave = async () => {
         try {
-            const response = await axios.post('http://localhost:5000/api/user/updateUser', {
-                userID: user.userID,
-                ...formData
-            });
+            const response = await axios.post(
+                "http://localhost:5000/api/user/updateUser",
+                {
+                    userID: user.userID,
+                    ...formData,
+                }
+            );
 
             const updatedUser = response.data.user;
-            localStorage.setItem('currentUser', JSON.stringify(updatedUser));
+            localStorage.setItem("currentUser", JSON.stringify(updatedUser));
             setUser(updatedUser);
             setIsEditing(false);
 
             // Show success message
-            message.success('Profile updated successfully');
+            message.success("Profile updated successfully");
         } catch (error) {
-            console.error('Failed to update user', error);
+            console.error("Failed to update user", error);
 
             // Show error message
-            message.error('Failed to update profile. Please try again.');
+            message.error("Failed to update profile. Please try again.");
         }
     };
 
@@ -64,20 +77,61 @@ function UserProfilePage() {
         <div className={`user-profile-page-1234`}>
             <div className={`profile-card-1234`}>
                 <div className={`profile-header-1234`}>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/1200px-Windows_10_Default_Profile_Picture.svg.png" alt="Profile" />
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/1200px-Windows_10_Default_Profile_Picture.svg.png"
+                        alt="Profile"
+                    />
                     <div className={`profile-info-1234`}>
-                        <h3>{user.firstName} {user.lastName}</h3>
+                        <h3>
+                            {user.firstName} {user.lastName}
+                        </h3>
                         <p>{user.userType}</p>
                     </div>
                 </div>
                 <div className={`profile-details-1234`}>
-                    <p><strong>Email:</strong> {user.email}</p>
-                    <p><strong>Username:</strong> {user.username}</p>
+                    <p>
+                        <strong>Email:</strong> {user.email}
+                    </p>
+                    <p>
+                        <strong>Username:</strong> {user.username}
+                    </p>
                 </div>
-                <button className={`edit-button-1234`} onClick={handleEdit}>Edit Profile</button>
+                <button className={`edit-button-1234`} onClick={handleEdit}>
+                    Edit Profile
+                </button>
             </div>
             <div className="additional-features-container-1234">
-
+                <ConfigProvider
+                    theme={{
+                        token: {
+                            colorPrimary: "#27ae61", // Set your desired primary color
+                        },
+                    }}
+                >
+                    <Tabs
+                        defaultActiveKey="1"
+                        onChange={onTabChange}
+                        activeKey={activeTabKey} // Bind active tab key
+                    >
+                        <Tabs.TabPane tab="Rooms" key="1">
+                            <RoomBookings />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="Events" key="2">
+                            <EventBookings />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="Foods" key="3">
+                            <FoodOrders />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="Parkings" key="4">
+                            <ParkingBookings />
+                        </Tabs.TabPane>
+                        {user.userType === "Employee" && (
+                            <Tabs.TabPane tab="Leaves" key="5">
+                                <LeaveDetails />
+                            </Tabs.TabPane>
+                        )}
+                    </Tabs>
+                </ConfigProvider>
             </div>
 
             {isEditing && (
@@ -86,23 +140,53 @@ function UserProfilePage() {
                         <h3>Edit Profile</h3>
                         <label>
                             First Name:
-                            <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} />
+                            <input
+                                type="text"
+                                name="firstName"
+                                value={formData.firstName}
+                                onChange={handleInputChange}
+                            />
                         </label>
                         <label>
                             Last Name:
-                            <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} />
+                            <input
+                                type="text"
+                                name="lastName"
+                                value={formData.lastName}
+                                onChange={handleInputChange}
+                            />
                         </label>
                         <label>
                             Email:
-                            <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+                            <input
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleInputChange}
+                            />
                         </label>
                         <label>
                             Username:
-                            <input type="text" name="username" value={formData.username} onChange={handleInputChange} />
+                            <input
+                                type="text"
+                                name="username"
+                                value={formData.username}
+                                onChange={handleInputChange}
+                            />
                         </label>
                         <div className={`modal-buttons-1234`}>
-                            <button className={`save-button-1234`} onClick={handleSave}>Save</button>
-                            <button className={`cancel-button-1234`} onClick={() => setIsEditing(false)}>Cancel</button>
+                            <button
+                                className={`save-button-1234`}
+                                onClick={handleSave}
+                            >
+                                Save
+                            </button>
+                            <button
+                                className={`cancel-button-1234`}
+                                onClick={() => setIsEditing(false)}
+                            >
+                                Cancel
+                            </button>
                         </div>
                     </div>
                 </div>
