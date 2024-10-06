ced0a47e6545b2656c1044f334d5ea11436ee770
Kaveen Dimantha <kaveendimantha@gmail.com>
2024-10-06 14:32:02 +0530

    SG | Add CSS File for Manage Feedback Admin

--- client/src/components/admin/ManageFeedbacks.jsx
+++ client/src/components/admin/ManageFeedbacks.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ManageFeedbacks() {
-    return <div>
+    return <div className="manage_feedback_main_container_admin">
         <h1>Manage Feedbacks</h1>
     </div>;
 }
